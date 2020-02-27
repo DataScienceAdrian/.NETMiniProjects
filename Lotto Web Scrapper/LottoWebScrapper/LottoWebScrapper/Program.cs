3e99d72e7f55c27dc0bdd5a0c79da292ece02a75
DataScienceAdrian <amarcinczyk95@gmail.com>
2020-02-27 22:33:24 +0100

    Basic scheme for web scrapper added

---
+++ Lotto Web Scrapper/LottoWebScrapper/LottoWebScrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LottoWebScrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
