50366b0ca8977a2fa4e79397524237f322699fe1
DataScienceAdrian <amarcinczyk95@gmail.com>
2020-02-27 22:28:29 +0100

    Code cleared, folder name changed for Simple WPF Pizza ordering app

---
+++ Simple WPF Pizza Ordering App/WpfApplication1/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfApplication1
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Logika interakcji dla klasy App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        
+    }
+}
--- Simple WPF Pizza Ordering App/WpfApplication1/MainWindow.xaml.cs
+++ Simple WPF Pizza Ordering App/WpfApplication1/MainWindow.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace WpfApplication1
+﻿namespace WpfApplication1
 {
-    
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+
     public partial class MainWindow : Window
     {
 
@@ -45,15 +30,10 @@ namespace WpfApplication1
             {
                 poziom_sosu = "Ostry";
             }
-
-
                 lab1.Content = selected_pizza;
-
-
 
             if (Radio1.IsChecked == true)
             {
-
                 lab1.Content = selected_pizza + "\n" + "Rozmiar: Mały" + "\n" + "Poziom ostrości sosu: "  + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                 Koszt.Text = "15 zł ";
             }
@@ -61,7 +41,6 @@ namespace WpfApplication1
 
             if (Radio2.IsChecked == true)
             {
-
                 lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Duży" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                 Koszt.Text = "20 zł ";
             }
@@ -69,20 +48,16 @@ namespace WpfApplication1
 
             if (Radio3.IsChecked == true)
             {
-
                 lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mega" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                 Koszt.Text = "40 zł (w tym 5 zł dostawa) ";
             }
 
-
-
-
             if (Radio1.IsChecked == true && check1.IsChecked == true)
                 {
-
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mały" + "\n" + "Sos Pomidorowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "16 zł ";
                 }
+
                 if (Radio1.IsChecked == true && check2.IsChecked == true)
                 {
 
@@ -95,8 +70,6 @@ namespace WpfApplication1
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mały" + "\n" + "Sos Pomidorowy" + "\n" + "Sos Czosnkowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "17 zł )";
                 }
-
-
 
                 if (Radio2.IsChecked == true && check1.IsChecked == true)
                 {
@@ -112,45 +85,30 @@ namespace WpfApplication1
                 }
                 if (Radio2.IsChecked == true && check2.IsChecked == true && check1.IsChecked == true)
                 {
-
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Duży" + "\n" + "Sos Pomidorowy" + "\n" + "Sos Czosnkowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "22 zł";
                 }
 
-
                 if (Radio3.IsChecked == true && check1.IsChecked == true)
                 {
-
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mega" + "\n" + "Sos Pomidorowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "41 zł (W tym dostawa 5 zł)";
                 }
                 if (Radio3.IsChecked == true && check2.IsChecked == true)
                 {
-
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mega" + "\n" + "Sos Czosnkowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "41 zł (W tym dostawa 5 zł)";
                 }
                 if (Radio3.IsChecked == true && check2.IsChecked == true && check1.IsChecked == true)
                 {
-
                     lab1.Content = "Rodzaj: " + selected_pizza + "\n" + "Rozmiar: Mega" + "\n" + "Sos Pomidorowy" + "\n" + "Sos Czosnkowy" + "\n" + "Poziom ostrości sosu: " + poziom_sosu + "\n" + "Uwagi: " + uwagi;
                     Koszt.Text = "42 zł (W tym dostawa 5 zł)";
                 }
-
-                
-
-
-
-
-
-
-           
-
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            
+         
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -158,16 +116,11 @@ namespace WpfApplication1
             int val = Convert.ToInt32(e.NewValue);
             string msg = String.Format("Poziom Ostrości Sosu: {0}", val);
             this.SliderText.Text = msg;
-
-
-
         }
 
         private void Radio1_Checked(object sender, RoutedEventArgs e)
         {
             Zamowienie.IsEnabled = true;
-
-          
         }
 
         private void Radio2_Checked(object sender, RoutedEventArgs e)
